f643c5f264311a1cbb0340e0c7b374a5546a9607
Justyna Wojtczak <justine84@gmail.com>
2019-03-27 18:41:59 +0100

    Seed I/O database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Actor.create!([
+  {name: "Jodie", surname: "Foster", birthDate: "1962-11-19"},
+  {name: "Anthony", surname: "Hopkins", birthDate: "1937-12-31"},
+  {name: "Matthew", surname: "McConaughey", birthDate: "1969-11-04"},
+  {name: "Anne", surname: "Hathaway", birthDate: "1982-11-12"},
+  {name: "Mackenzie", surname: "Foy", birthDate: "2000-11-10"}
+])
+Country.create!([
+  {name: "Polska"},
+  {name: "Anglia"},
+  {name: "Niemcy"},
+  {name: "Francja"},
+  {name: "USA"},
+  {name: "Włochy"}
+])
