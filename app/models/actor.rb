9d5b3f41182a3a7c0c08c67bf36ff168e52dd26a
Justyna Wojtczak <justine84@gmail.com>
2019-03-27 18:55:28 +0100

    Add uniqueness validation to models

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,5 @@
 class Actor < ApplicationRecord
   validates_presence_of :name, :surname, :birthDate
+  validates :name, uniqueness: { scope: :surname,
+                                 message: "should be unique with surname together" }
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,3 @@
 class Country < ApplicationRecord
-  validates_presence_of :name
+  validates :name, presence: true, uniqueness: true
 end
