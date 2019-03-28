2990506e99836523a63289e5bbe47287639c734f
Justyna Wojtczak <justine84@gmail.com>
2019-03-28 11:14:36 +0100

    Update actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,8 @@ class Actor < ApplicationRecord
   validates :name, uniqueness: { scope: :surname,
                                  message: "should be unique with surname together" }
   has_and_belongs_to_many :movies
+
+  def name_with_initial
+    "#{name.first}. #{surname}"
+  end
 end
